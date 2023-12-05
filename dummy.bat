c5cb17d5ec5417d1dbe38bdb69fb33fd974643bb
simon-komment <150942169+simon-komment@users.noreply.github.com>
2023-12-05 18:41:33 +0000

    Update dummy.bat

--- dummy.bat
+++ dummy.bat
@@ -1,14 +1,7 @@
-/**
-* @description The function `sum` takes two arguments `a` and `b` and returns their
-* sum.
-* 
-* @param {  } a - In the given function `sum`, the `a` input parameter is ignored.
-* 
-* @param { any } b - In the function `sum = (a., b) -> a + b`, the `b` input parameter
-* is used as the second operand to be added to the first operand `a`.
-*/
-let sum = (a, b) => a + b;
-
+@echo off
+title Dummy 
+echo Dummy
+pause
 
 
 
