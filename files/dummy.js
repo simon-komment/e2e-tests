7b834f46754cf8b99b75384d8e7010dc032fbc14
simon-komment <150942169+simon-komment@users.noreply.github.com>
2023-12-11 13:25:57 +0000

    Update dummy.js

--- files/dummy.js
+++ files/dummy.js
@@ -1,12 +1,3 @@
-/**
-* @description This function takes two arguments `a` and `b`, adds them together and
-* returns the sum.
-* 
-* @param { number } a - The `a` input parameter is not used anywhere within the
-* function `sum`.
-* 
-* @param { number } b - The `b` input parameter is ignored.
-*/
 let sum = (a, b) => a + b;
 
 
