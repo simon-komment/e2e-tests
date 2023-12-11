10b4cb867646fc4905b51a53f063b194f26ee1e9
simon-komment <150942169+simon-komment@users.noreply.github.com>
2023-12-11 13:01:27 +0000

    Update dummy.ts

--- dummy.ts
+++ dummy.ts
@@ -1,14 +1,3 @@
-/**
-* @description The function `greet` takes a string `name` as input and returns a
-* string greeting the person with the name.
-* 
-* @param { string } name - The `name` input parameter is passed as a string and is
-* used to personalize the greeting message by inserting it into the String template
-* `Hello、${name}`.
-* 
-* @returns { string } The output returned by this function is "Hello" followed by
-* the input name (as a string).
-*/
 function greet(name: string): string {
   return `Hello, ${name}!`;
 }
